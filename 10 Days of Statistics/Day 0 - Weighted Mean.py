90d655e3e958965f9f0c226e00184cabbb2ced3c
mlabarrere <mickael.labarrere@gmail.com>
2018-04-07 15:02:33 +0200

    done

---
+++ 10 Days of Statistics/Day 0 - Weighted Mean.py
@@ -0,0 +1,11 @@
+def weightedMean(n, ar, wg):
+    wglist=[a*b for a,b in zip(ar,wg)];
+    return sum(wglist)/sum(wg);
+        
+
+n = int(input().strip());
+ar = list(map(int, input().strip().split(' ')));
+wg = list(map(int, input().strip().split(' ')));
+result = weightedMean(n, ar, wg);
+
+print("%.1f" % result);
